90c18afb1eea473cc4cb104c359fee4b7c9fec41
celebrater <tkgm45nbchk@gmail.com>
2022-03-31 07:38:59 +0900

    updated inits

--- init.php
+++ init.php
@@ -6,3 +6,4 @@ define('PNLG_SITE_NAME', "PHP Number Link Generator");
 define('PNLG_INDEX_FILE_NAME', "index.php");
 define('PNLG_MAX_TEXT_NUM', 4); // 1ページに表示するテキストの最大数
 define('PNLG_MAX_LINK_NUM', 5); // 1ページに貼る数字リンクの最大数
+define('PNLG_HCM_PATH', "../html_common_module.php"); // html_common_module.php
--- modules/create_html.php
+++ modules/create_html.php
@@ -2,18 +2,19 @@
 
 namespace php_number_link_generator\modules;
 
+use common_modules as cm;
 use php_number_link_generator\classes\Article;
 use php_number_link_generator\classes\NumberLink;
 
 require_once(dirname(__FILE__) . '/../classes/NumberLink.php');
 require_once(dirname(__FILE__) . '/../classes/Article.php');
 require_once(dirname(__FILE__) . '/../init.php');
-require_once "html_common_module.php";
+require_once PNLG_HCM_PATH;
 
 function get_p_lines_html($lines){
     $html = "";
     foreach ($lines as $line){
-        $html .= space_br("<p>" . $line . "</p>", 2);
+        $html .= cm\space_br("<p>" . $line . "</p>", 2);
     }
     return $html;
 }
@@ -23,11 +24,11 @@ function get_articles_html($link, $articles){
     $html = "";
     for($i = $link->start; $i < $link->start + PNLG_MAX_TEXT_NUM; $i++){
         if($i < $link->text_sum){
-            $html .= space_br("<hr>", 1);
-            $html .= space_br("<h2>" . $articles[$i]->title . "</h2>", 1);
-            $html .= space_br("<div>", 1);
+            $html .= cm\space_br("<hr>", 1);
+            $html .= cm\space_br("<h2>" . $articles[$i]->title . "</h2>", 1);
+            $html .= cm\space_br("<div>", 1);
             $html .= get_p_lines_html($articles[$i]->lines);
-            $html .= space_br("</div>", 1);
+            $html .= cm\space_br("</div>", 1);
         }
     }
     return $html;
@@ -56,10 +57,10 @@ function pnlg_get_html(){
     $file_names = glob('files/*.txt'); // ファイル名一覧
     $link = get_link($file_names);
     $articles = get_articles($file_names);
-    $html = space_br("<p>page_num: " . $link->page_num . "</p>", 1);
-    $html .= space_br("<p>current_link_page: " . $link->current_link_page . "</p>", 1);
-    $html .= space_br("<p>start_page_num: " . $link->start_page_num . "</p>", 1);
-    $html .= space_br("<h1>" . PNLG_SITE_NAME . "</h1>", 1);
+    $html = cm\space_br("<p>page_num: " . $link->page_num . "</p>", 1);
+    $html .= cm\space_br("<p>current_link_page: " . $link->current_link_page . "</p>", 1);
+    $html .= cm\space_br("<p>start_page_num: " . $link->start_page_num . "</p>", 1);
+    $html .= cm\space_br("<h1>" . PNLG_SITE_NAME . "</h1>", 1);
     $html .= get_articles_html($link, $articles);
     $html .= $link->get_page_links_html("");
     return $html;
